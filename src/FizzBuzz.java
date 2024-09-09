f83c3c0bf376b46750735e6eda6670b40ff0a049
Barış <bayazitbaris@gmail.com>
2024-09-09 09:57:12 -0400

    Refactor doFizzBuzz to take an int input

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,26 +4,24 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
-        doFizzBuzz();
-    }
-
-    private static void doFizzBuzz() {
         int i = 1;
         while (i < 100) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-            if (divisibleBy3 && divisibleBy5) {
-                System.out.println("Fizz Buzz");
-            } else if (divisibleBy3) {
-                System.out.println("Fizz");
-            } else if (divisibleBy5) {
-                System.out.println("Buzz");
-
-            } else {
-                System.out.println(i);
-            }
-
+            doFizzBuzz(i);
             i += 1;
+        }
+    }
+
+    private static void doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(i);
         }
     }
 }
